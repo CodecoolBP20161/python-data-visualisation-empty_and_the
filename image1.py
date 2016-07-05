cd3dfb7432fa715bf65a9fe7c593aa529ebcd29f
Benedek Vörös <xbenedek01@gmail.com>
2016-07-05 15:24:39 +0200

    temporary change for practicing

--- image1.py
+++ image1.py
@@ -1,14 +1,15 @@
 from PIL import Image
 from PIL import ImageDraw
 from csv_handler import CSVFile
-from sql_handler import DBconnection
+from sql_handler import Databases
 
 csv_file = CSVFile("admin_data.csv")
 list_data = csv_file.get_data()
-database = DBconnection(list_data[0], list_data[1], list_data[2], list_data[3])
-database.connect_sql()
-sometext = database.run_sql("""SELECT company_name FROM project WHERE id = 1;""")
-somenumber = database.run_sql("""SELECT budget_value FROM project WHERE id = 1;""")
+database = Databases(list_data[0], list_data[1], list_data[2], list_data[3])
+database.connect_db()
+Databases.create_table('base_data.sql')
+sometext = database.run_sql_command("""SELECT company_name FROM project WHERE id = 1;""")
+somenumber = database.run_sql_command("""SELECT budget_value FROM project WHERE id = 1;""")
 
 img = Image.new("RGB", (512, 512), "red")
 draw = ImageDraw.Draw(img)
