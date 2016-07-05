5fdb13e203f770ff6d6cd38b7d5387c145d63662
Benedek Vörös <xbenedek01@gmail.com>
2016-07-05 18:43:17 +0200

    init added

--- colors.py
+++ colors.py
@@ -1,7 +1,9 @@
 class Colors:
 
-    @staticmethod
-    def hex_to_rgb(hex_code):
-        long_hex_code = list(hex_code)[1] * 2 + list(hex_code)[2] * 2 + list(hex_code)[3] * 2
+    def __init__(self, hex_code):
+        self.hex_code = hex_code
+
+    def hex_to_rgb(self):
+        long_hex_code = list(self.hex_code)[1] * 2 + list(self.hex_code)[2] * 2 + list(self.hex_code)[3] * 2
         lv = len(long_hex_code)
         return tuple(int(long_hex_code[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
