44fa3e5af2790d41133705fb63444b2c9cbd939f
Benedek Vörös <xbenedek01@gmail.com>
2016-07-05 14:10:56 +0200

    handling csv files

---
+++ csv_handler.py
@@ -0,0 +1,16 @@
+import csv
+
+
+class CSVFile:
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.list_data = []
+
+    def get_data(self):
+        with open(self.filename, 'r') as csv_file:
+            reader = csv.reader(csv_file, delimiter=';')
+            list_csv = list(reader)
+            for element in list_csv:
+                self.list_data.append(element[1])
+            return self.list_data
