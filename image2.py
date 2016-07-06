7d35efd416e24f0733e135f5dca1f56927d17e06
Benedek Vörös <xbenedek01@gmail.com>
2016-07-06 15:08:32 +0200

    temporary changes for practicing

--- image2.py
+++ image2.py
@@ -3,21 +3,29 @@ from PIL import ImageDraw
 from PIL import ImageFont
 from csvfiles import CSVfiles
 from databases import Databases
+from projects import Projects
 
 csv_file = CSVfiles("admin_data.csv")
 list_data = csv_file.get_data()
 database = Databases(list_data[0], list_data[1], list_data[2], list_data[3])
 database.connect_db()
 Databases.create_table('base_data.sql')
-sometext = database.run_sql_command("""SELECT company_name FROM project WHERE id = 1;""")
-somenumber = database.run_sql_command("""SELECT name FROM project WHERE id = 3;""")
+list_from_sql = database.run_sql_command("""SELECT name, main_color, budget_value, budget_currency FROM project
+WHERE name = 'Voltsillam';""")
+list_of_arguments = ["name", "main_color", "budget_value", "budget_currency"]
+projects_object_list = Projects.get_projects_object_list(list_of_arguments, list_from_sql)
+for element in projects_object_list:
+    print(element.name, element.budget_eur, element.main_color)
+
+
+
 
 img = Image.new("RGB", (500, 500), "white")
 draw = ImageDraw.Draw(img)
 # font = ImageFont.truetype(<font-file>, <font-size>)
 font = ImageFont.truetype("arial.ttf", 16)
 text_options = {
-    'fill': (0, 0, 0)
+    'fill': element.main_color
 }
 text_content = "Sample Text"
 text_size = font.getsize(text_content)
