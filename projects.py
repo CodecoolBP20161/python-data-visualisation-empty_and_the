9290ebbc5dd59049808fda535d446d9f048c6678
Benedek Vörös <xbenedek01@gmail.com>
2016-07-06 15:07:31 +0200

    names changed

--- projects.py
+++ projects.py
@@ -4,13 +4,13 @@ from budgets import Budgets
 
 class Projects:
 
-    def __init__(self, name, duedate=None, company_name=None, company_hq=None, maincolor=None, manager=None,
+    def __init__(self, name, duedate=None, company_name=None, company_hq=None, main_color=None, manager=None,
                  budget_value=None, budget_currency=None, status=None, maintenance_requested=None):
         self.name = name
         self.duedate = duedate
         self.company_name = company_name
         self.company_hq = company_hq
-        self.maincolor = Colors(maincolor).hex_to_rgb()
+        self.main_color = Colors(main_color).hex_to_rgb()
         self.manager = manager
         self.budget_value = budget_value
         self.budget_currency = budget_currency
@@ -24,7 +24,7 @@ class Projects:
     @staticmethod
     def get_object_from_dictionary(kwargs):
         return Projects(name=kwargs.get('name'), duedate=kwargs.get('duedate'), company_name=kwargs.get('company_name'),
-                        company_hq=kwargs.get('company_hq'), maincolor=kwargs.get('maincolor'),
+                        company_hq=kwargs.get('company_hq'), main_color=kwargs.get('main_color'),
                         manager=kwargs.get('manager'), budget_value=kwargs.get('budget_value'),
                         budget_currency=kwargs.get('budget_currency'), status=kwargs.get('status'),
                         maintenance_requested=kwargs.get('maintenance_requested'))
